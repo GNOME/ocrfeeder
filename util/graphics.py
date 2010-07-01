9546c8c075f57af14eb4516c68a729dce74cf624
Joaquim Rocha <jrocha@igalia.com>
2010-07-01 10:53:21 +0200

    graphics: Delete trailing whitespaces

--- util/graphics.py
+++ util/graphics.py
@@ -3,7 +3,7 @@
 ###########################################################################
 #    OCRFeeder - The complete OCR suite
 #    Copyright (C) 2009 Joaquim Rocha
-# 
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
