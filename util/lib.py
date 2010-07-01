8cf75c93e03621a89a9238e03828dafbe74c4d82
Joaquim Rocha <jrocha@igalia.com>
2010-07-01 10:53:44 +0200

    lib: Delete trailing whitespaces

--- util/lib.py
+++ util/lib.py
@@ -3,7 +3,7 @@
 ###########################################################################
 #    OCRFeeder - The complete OCR suite
 #    Copyright (C) 2009 Joaquim Rocha
-# 
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -80,7 +80,7 @@ def getDictFromVariables(variable_list, instance):
     return dictionary
 
 def getNonExistingFileName(file_name):
-    
+
     if not os.path.exists(file_name):
         return file_name
     name, ext = os.path.splitext(file_name)
