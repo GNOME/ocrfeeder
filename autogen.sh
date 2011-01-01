ad62e880fa94434d20147097b2c9a35d782dba7b
Joaquim Rocha <jrocha@igalia.com>
2011-01-01 17:15:10 +0100

    Added autotools
    Now the build is managed by autotools

---
+++ autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Calls gnome-autogen to build Makefiles and run configure
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="ocrfeeder"
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common module and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
+    exit 1
+}
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
